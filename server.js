95f4e9b8768f6a1831292c947d130a2326af77be
System <none>
2017-06-30 19:00:38 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -13,9 +15,11 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  console.log(request.query.case);
+  console.log(request.body);
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.post("/", function re)
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
